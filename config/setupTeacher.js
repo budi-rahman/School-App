fcb9458907f5f64b3a5a7df1491e75ef65866e25
Rofi Alfarid <rofi.alfarid@ovo.id>
2020-12-10 19:52:00 +0700

    remove comma hungkul bud

--- config/setupTeacher.js
+++ config/setupTeacher.js
@@ -1,3 +1,4 @@
+
 const pool = require("./connection")
 
 const teacher_table = `CREATE TABLE "Teacher" (
@@ -5,9 +6,9 @@ const teacher_table = `CREATE TABLE "Teacher" (
 	first_name VARCHAR ( 50 ) NOT NULL,
 	last_name VARCHAR ( 50 ) NOT NULL,
 	email VARCHAR ( 50 ) NOT NULL,
-	gender VARCHAR (20) NOT NULL,
+	gender VARCHAR (20) NOT NULL
 );`
 
 pool.query(teacher_table, () => {
-     
+
 })
